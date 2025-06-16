fd0983130c559769bba7e5f7782f02743ae5f2ec
Panos <panossmav@icloud.com>
2025-06-16 23:49:54 +0300

    Add db files

--- database.py
+++ database.py
@@ -8,10 +8,17 @@ with open('commands.sql','r') as f:
     sql_script = f.read()
 cursor.executescript(sql_script)
 conn.commit()
-conn.close()
+
 
 def add_users(name,age,dob,email):
+    conn = sql.connect('database.db')
+    cursor = conn.cursor()
     cursor.execute( "INSERT INTO users (name,age,dob,email) VALUES(?, ?, ?, ?)",
     (name,age,dob,email)
 
     )
+    conn.commit()
+    conn.close()
+
+conn.close()
+    
